94dc778b0ab54564816217f6bc8b88ad0cd6e919
tudao@5nhatnhat.com <nevarda9416>
2021-10-01 16:59:33 +0700

    - [VueJS]: Call API data from vue file

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,38 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <div v-for="post in posts">
+      <h2>{{post.title}}</h2>
+      <p>{{post.body}}</p>
+    </div>
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      posts: []
+    }
+  },
+  methods: {
+    async getData () {
+      try {
+        const response = await this.$http.get(
+          "http://jsonplaceholder.typicode.com/posts"
+        )
+        // JSON responses are automatically parsed.
+        this.posts = response.data
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  },
+  mounted () {
+    this.getData()
+  }
 }
 </script>
 
