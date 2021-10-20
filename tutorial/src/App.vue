fe7e8deb22d1dfd92f89bf1a367a88b9d4fed99d
tudao@5nhatnhat.com <nevarda9416>
2021-10-20 15:20:27 +0700

    - [VueJS Tutorial]: Features available + Virtual DOM + Data Binding + Components + Event Handling (v-on: listen to the events) + Animation/Transition + Computed properties + Templates + Directives (v-if, v-else, v-show, v-bind, v-model) + Watchers + Routing

---
+++ tutorial/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+@import url("https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css");
+@import url("https://fonts.googleapis.com/css2?family=Poppins&display=swap");
+@import url("https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/slate/bootstrap.min.css");
+@import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css");
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
