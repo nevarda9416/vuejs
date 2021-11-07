4d153f4de874ac29e287bbbf58d363e567216509
tudao@5nhatnhat.com <nevarda9416>
2021-11-07 09:33:50 +0700

    - [VueJS]: Install vuejs in laravel project

---
+++ laravelvuejsproduct/resources/assets/js/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <div>
+            <transition name="fade">
+                <route-view></route-view>
+            </transition>
+        </div>
+    </div>
+</template>
+<style>
+.fade-enter-active, .fade-leave-active {
+    transition: opacity .5s
+}
+.fade-enter, .fade-leave-active {
+    opacity: 0
+}
+</style>
+<script>
+export default {
+    
+}
+</script>
