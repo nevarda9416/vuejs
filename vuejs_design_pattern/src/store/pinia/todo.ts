2e87a91e8180274b90dce7ff148b4badbbb12215
TuDT <tudt@codluck.com>
2023-07-05 13:05:23 +0700

    State Management Pattern

---
+++ vuejs_design_pattern/src/store/pinia/todo.ts
@@ -0,0 +1,24 @@
+import type { Todo } from "./../types/todo";
+import { defineStore } from "pinia";
+
+export const useTodoStore = defineStore("todos", {
+  state: () => {
+    return {
+      list: [] as Todo[],
+    }
+  },
+  getters: {
+    getList: (store) => {
+      return store.list;
+    },
+  },
+  actions: {
+    addTodo(todo: Todo) {
+      this.list = [todo, ...this.list];
+    },
+    async fetchTodos() {
+      const data = await fetch("https://jsonplaceholder.typicode.com/todos");
+      this.list = await data.json();
+    },
+  },
+});
---
+++ vuejs_design_pattern/src/store/vuex/index.ts
@@ -0,0 +1,12 @@
+import { createStore } from "vuex";
+import todoStore from "./todo";
+
+const store = createStore({
+  modules: {
+    todo: {
+      namespaced: true,
+      ...todoStore,
+    }
+  }
+});
+export default store;
